20cd2a0e2e61eef2e38e7b89ba99d8a141eeb871
Ariel Liu <44077856+arielycliu@users.noreply.github.com>
2021-05-31 08:55:17 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,13 +2,13 @@
 # print("You think there might be something useful in this room that can help break you out of your handcuffs")
 # print("Take a look around")
 
-import Commands
+# import Commands
 
-cur_place = "cell"
-money = 20
+# cur_place = "cell"
+# money = 20
 
-while True:
-    cur_place, money = Commands.basic_commands(cur_place, money)
+# while True:
+#     cur_place, money = Commands.basic_commands(cur_place, money)
 
-
+# DO NOT RUN ME USE TEST INTRO.PY INSTEAD
 
