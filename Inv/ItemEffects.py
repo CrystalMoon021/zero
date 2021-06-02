05fc429fc6ad4f1fa943cd295d9d059d23ad37ef
arielycliu <ariel.yc.liu@gmail.com>
2021-06-02 10:08:40 -0400

    Ensure that user must take pen

--- Inv/ItemEffects.py
+++ Inv/ItemEffects.py
@@ -1,6 +1,8 @@
 
 from Obj import Look
 from Obj import Diary
+from Hid import Hidden
+from Inv import Inventory
 import Commands
 
 def special_check(item, cur_place): # return new location to update cur_place
@@ -9,9 +11,17 @@ def special_check(item, cur_place): # return new location to update cur_place
         return cur_place
     elif item == "uniform":
         print("The foundation's collector walks in on you straightening your tie. ")
-        print("'What is a guard like you doing here?' he squints in suspicion, 'Are you trying to avoid doing your job?' ")
+        print("'What is a guard like you doing here?' he squints in suspicion, 'Are you trying to slack off?' ")
+        Bag = Inventory.read_inventory()
+        try:
+            pen = Bag["pen"]
+        except:
+            print("'This isn't your personal quarters! Pick up your belongings' he picks up the pen and slaps it onto your hand.")
+            Hidden.hidden_obj_unlocked("pen")
+
         print("He ushers you out of the office and opens the front door, 'Go out and do what you're paid for!' he exclaims shoving you out of the building. ")
         Look.unlocked.extend(["church", "house", "store", "town"])
+
         return "town" # change location to town
     elif item == "pen":
         Diary.write_in_diary("diary", cur_place)
