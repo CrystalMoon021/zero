076d9f907b0a771decfecd0da64b352b350bbab3
arielycliu <ariel.yc.liu@gmail.com>
2021-06-07 23:51:22 -0400

    minor changes about the uniform pickup functions

--- MAIN.py
+++ MAIN.py
@@ -20,7 +20,7 @@ cur_place, money = Commands.basic_commands("break cross", cur_place, money)
 cur_place, money = Commands.basic_commands("use wires", cur_place, money)
 cur_place, money = Commands.basic_commands("go to office", cur_place, money)
 cur_place, money = Commands.basic_commands("pick up uniform", cur_place, money)
-cur_place, money = Commands.basic_commands("use uniform", cur_place, money)
+cur_place, money = Commands.basic_commands("go to cell", cur_place, money)
 cur_place, money = Commands.basic_commands("wear uniform", cur_place, money)
 
 while True:
